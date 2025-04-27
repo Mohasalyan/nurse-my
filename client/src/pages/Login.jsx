0f672a9ca3a49d59939419821a15613c065e9ee1
Qussay abu snana <Qussay0527405139@gmail.com>
2025-04-27 15:56:16 +0300

    Login page but should make responsive

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,25 +1,27 @@
 // src/pages/Login.jsx
 import React from 'react';
-import './Login.css'; // We will create this file
-import loginImage from '../assets/login-illustration.png'; // update the path
+import './Login.css';
+import loginImage from '../assets/login-illustration.png';
 
 const Login = () => {
   return (
     <div className="login-container">
-      {/* Left Side - Image */}
+      {/* Left - Image */}
       <div className="login-image">
         <img src={loginImage} alt="Login Illustration" />
       </div>
 
-      {/* Right Side - Form */}
+      {/* Right - Form */}
       <div className="login-form">
-        <h1>עמותת ותיקי מטה יהודה</h1>
-        <p>הזדהות וכניסה למערכת !</p>
-        <form>
-          <input type="text" placeholder="שם משתמש" />
-          <input type="password" placeholder="סיסמה" />
-          <button type="submit">כניסה</button>
-        </form>
+        <div className="login-form-content">
+          <h1>עמותת ותיקי מטה יהודה</h1>
+          <p>הזדהות וכניסה למערכת !</p>
+          <form>
+            <input type="text" placeholder="שם משתמש" />
+            <input type="password" placeholder="סיסמה" />
+            <button type="submit">כניסה</button>
+          </form>
+        </div>
       </div>
     </div>
   );
