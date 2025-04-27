5f2d3d3310522edad56d25fa5146999d3726e864
Qussay abu snana <Qussay0527405139@gmail.com>
2025-04-27 18:11:06 +0300

    still need some finishes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,27 +1,64 @@
-// src/pages/Login.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import loginImage from '../assets/login-illustration.png';
+import { FaUser, FaLock } from 'react-icons/fa';
+import { AiOutlineEye, AiOutlineEyeInvisible, AiOutlineCloseCircle } from 'react-icons/ai';
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const clearUsername = () => {
+    setUsername('');
+  };
+
   return (
     <div className="login-container">
-      {/* Left - Image */}
       <div className="login-image">
         <img src={loginImage} alt="Login Illustration" />
       </div>
 
-      {/* Right - Form */}
       <div className="login-form">
-        <div className="login-form-content">
-          <h1>עמותת ותיקי מטה יהודה</h1>
-          <p>הזדהות וכניסה למערכת !</p>
-          <form>
-            <input type="text" placeholder="שם משתמש" />
-            <input type="password" placeholder="סיסמה" />
-            <button type="submit">כניסה</button>
-          </form>
-        </div>
+        <h1>עמותת ותיקי מטה יהודה</h1>
+        <p> !הזדהות וכניסה למערכת </p>
+        <form>
+          {/* Username Field */}
+          <div className="input-container">
+            <input 
+              type="text" 
+              placeholder="שם משתמש" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <FaUser className="input-icon" />
+            {username && (
+              <span className="clear-input" onClick={clearUsername}>
+                <AiOutlineCloseCircle />
+              </span>
+            )}
+          </div>
+
+          {/* Password Field */}
+          <div className="input-container">
+            <input 
+              type={showPassword ? "text" : "password"} 
+              placeholder="סיסמה" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <FaLock className="input-icon" />
+            <span className="toggle-password" onClick={togglePassword}>
+              {showPassword ? <AiOutlineEyeInvisible /> : <AiOutlineEye />}
+            </span>
+          </div>
+
+          <button type="submit">כניסה</button>
+        </form>
       </div>
     </div>
   );
