1cebcfb4f442c0d4e3a2ca21429ad4f31f64b7dc
Nagham <naghammhesen@gmail.com>
2025-04-29 16:19:59 +0300

    edited the card component styling

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,16 +1,15 @@
 // src/components/Card.jsx
+// src/components/Card.jsx
 import React from "react";
-import "./Card.css"; // سنحتاج كمان ملف ستايل خاص بالكرت لو أردت
+import "./Card.css";
 
-const Card = ({ image, title, color = "#7bb08e" }) => {
+const Card = ({ image, title, color = "#7bb08e", textcolor = "#FFFFFF" }) => {
   return (
     <div className="card" style={{ backgroundColor: color }}>
       <img src={image} alt={title} className="card-image" />
-      <h3 className="card-title">{title}</h3>
+      <h3 className="card-title" style={{ color: textcolor }}>{title}</h3>
     </div>
   );
 };
-
-
 
 export default Card;
