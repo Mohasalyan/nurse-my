be7fae5e873f78752a08d60777e515c9fc7d0865
Nagham <naghammhesen@gmail.com>
2025-04-29 15:38:52 +0300

    updated card component color and added patirnt records page

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -2,13 +2,15 @@
 import React from "react";
 import "./Card.css"; // سنحتاج كمان ملف ستايل خاص بالكرت لو أردت
 
-const Card = ({ image, title }) => {
+const Card = ({ image, title, color = "#7bb08e" }) => {
   return (
-    <div className="card">
+    <div className="card" style={{ backgroundColor: color }}>
       <img src={image} alt={title} className="card-image" />
       <h3 className="card-title">{title}</h3>
     </div>
   );
 };
+
+
 
 export default Card;
